10dbeb8688971999e9936cbda9a8155e4ff50a55
MaliugaArtsiom <63003946+Maliuga@users.noreply.github.com>
2024-06-20 17:52:42 +0300

    init

---
+++ src/components/StoryScreens/NoStories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NoStories = () => (
+  <div className="no-story">
+    <h2>Историй не найдено</h2>
+    <p>Попробуйте поискать иначе</p>
+  </div>
+);
+
+export default NoStories;
